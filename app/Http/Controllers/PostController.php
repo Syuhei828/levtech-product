a69201d13de921d2564646ce92ff18f02415db4f
Syuhei828 <Satosyu03@icloud.com>
2025-03-13 12:18:56 +0000

    Blog Post list disply

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//use宣言は外部にあるクラスをGuitarController内にインポートできる。
-//この場合、App\Models内のGuitarクラスをインポートしている。
+//use宣言は外部にあるクラスをPostController内にインポートできる。
+//この場合、App\Models内のPostクラスをインポートしている。
 use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function index(Post $post) //インポートしたGuitarをインスタンス化して$guitarとして使用。
+    public function index(Post $post) //インポートしたPostをインスタンス化して$postとして使用。
     {
-        return $post->get(); //$guitarの中身を戻り値にする。
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit(3)]); //$postの中身を戻り値にする。
+        //blade内で使う変数'posts'と設定。'posts'中身にgetを使い、インスタンス化した$postを代入。
     }
 }
